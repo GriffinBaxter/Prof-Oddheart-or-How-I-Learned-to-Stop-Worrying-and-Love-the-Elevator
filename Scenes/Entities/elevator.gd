ef925412262f1cd6341af19fce5927b324558895
keagan erasmus <keaganerasmus12@gmail.com>
2025-04-12 13:00:09 +1200

    FIX: Pressing escape now uncaptures mouse

--- Scenes/Entities/elevator.gd
+++ Scenes/Entities/elevator.gd
@@ -6,6 +6,9 @@ extends CharacterBody3D
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _physics_process(_delta: float) -> void:
 	var input := Input.get_vector("left", "right", "down", "up")
