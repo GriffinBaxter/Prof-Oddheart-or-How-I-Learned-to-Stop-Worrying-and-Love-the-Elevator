74afe6ffde08eac6cfd259495326430dc0a057e1
Griffin Baxter <47467946+GriffinBaxter@users.noreply.github.com>
2025-04-12 12:22:57 +1200

    $112: Change back and forwards to mouse controls

--- Scenes/Entities/elevator.gd
+++ Scenes/Entities/elevator.gd
@@ -3,21 +3,20 @@ extends CharacterBody3D
 @export var speed: float = 10.
 
 
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
 func _physics_process(_delta: float) -> void:
-	var input := Input.get_vector("left", "right", "forward", "backward")
-	var direction := transform.basis * Vector3(input.x, 0, input.y).normalized()
+	var input := Input.get_vector("left", "right", "down", "up")
+	var direction := transform.basis * Vector3(input.x, input.y, 0).normalized()
 	if direction:
 		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
+		velocity.y = direction.y * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
-
-	if Input.is_action_pressed("up"):
-		velocity.y = speed
-	elif Input.is_action_pressed("down"):
-		velocity.y = -speed
-	else:
 		velocity.y = move_toward(velocity.y, 0, speed)
+
+	velocity.z = speed * Input.get_last_mouse_velocity().y * 0.001
 
 	move_and_slide()
