3c4ebcec57164cae01a698a39c044cff49fb5ed1
Griffin Baxter <47467946+GriffinBaxter@users.noreply.github.com>
2025-04-13 19:48:59 +1200

    $11s: Add colours to people and corridors

--- Scenes/Entities/person_base.gd
+++ Scenes/Entities/person_base.gd
@@ -1,16 +1,25 @@
 extends RigidBody3D
 
+const COLOURS = [Color.RED, Color.GREEN, Color.BLUE]
+
 @export var speed: float = 5
 @export var fall_speed: float = 1
 
 var in_elevator: bool = false
 var direction: int = 1
+var colour: Color
 
 @onready var left_ray: RayCast3D = $CollisionRayLeft
 @onready var right_ray: RayCast3D = $CollisionRayRight
+@onready var body: CSGMesh3D = $Body
+
 
 func _ready() -> void:
 	direction = randi_range(-1, 1)
+	var material = StandardMaterial3D.new()
+	material.albedo_color = COLOURS[randi_range(0, 2)]
+	body.material = material
+
 
 func _process(_delta: float) -> void:
 	if left_ray.is_colliding():
@@ -21,6 +30,7 @@ func _process(_delta: float) -> void:
 		var right_coll = right_ray.get_collider()
 		if right_coll.is_in_group("outer_walls"):
 			direction = direction * -1
+
 
 func _physics_process(delta: float) -> void:
 	if !in_elevator:
--- Scenes/corridor.gd
+++ Scenes/corridor.gd
@@ -1,5 +1,15 @@
 extends Node3D
 
+const COLOURS = [Color.RED, Color.GREEN, Color.BLUE]
+@onready var corridor_boxes: Array[CSGBox3D] = [$Back, $Top, $Bottom]
+
+
+func _ready() -> void:
+	var material = StandardMaterial3D.new()
+	material.albedo_color = COLOURS[randi_range(0, 2)]
+	for box in corridor_boxes:
+		box.material = material
+
 
 func _on_drop_off_area_body_entered(body: Node3D) -> void:
 	if body.is_in_group("people") and body.in_elevator:
