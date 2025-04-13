7b62ecba41a9f618141992b8d26ba85d8e986974
keagan erasmus <keaganerasmus12@gmail.com>
2025-04-13 18:58:07 +1200

    FEAT: I clicked use collisions this caused big problem

--- Scenes/Entities/person_base.gd
+++ Scenes/Entities/person_base.gd
@@ -1,13 +1,16 @@
 extends RigidBody3D
 
-@export var speed: float = 0.05
+@export var speed: float = 5
+@export var fall_speed: float = 1
 
 var in_elevator: bool = false
-var direction = 1
+var direction: int = 1
 
 @onready var left_ray: RayCast3D = $CollisionRayLeft
 @onready var right_ray: RayCast3D = $CollisionRayRight
 
+func _ready() -> void:
+	direction = randi_range(-1, 1)
 
 func _process(_delta: float) -> void:
 	if left_ray.is_colliding():
@@ -18,7 +21,6 @@ func _process(_delta: float) -> void:
 		var right_coll = right_ray.get_collider()
 		if right_coll.is_in_group("outer_walls"):
 			direction = direction * -1
-
 
 func _physics_process(delta: float) -> void:
 	if !in_elevator:
--- Scenes/character_spawner.gd
+++ Scenes/character_spawner.gd
@@ -10,7 +10,6 @@ extends Node3D
 func _ready() -> void:
 	$CSGMesh3D.hide()
 
-
 func _on_spawn_timer_timeout() -> void:
 	var character = character_scene.instantiate()
 	timer.wait_time = randf_range(min_spawn_time, max_spawn_time)
