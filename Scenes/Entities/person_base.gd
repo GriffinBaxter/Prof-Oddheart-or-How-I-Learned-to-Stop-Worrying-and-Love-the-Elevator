762a37d6ce9579cdd5e356eb7b6f4f6b41d3c6a5
Griffin Baxter <47467946+GriffinBaxter@users.noreply.github.com>
2025-04-18 15:02:58 +1200

    Add elevator ding
    Sound came from https://pixabay.com/sound-effects/ (royalty-free, no attribution required)

--- Scenes/Entities/person_base.gd
+++ Scenes/Entities/person_base.gd
@@ -11,6 +11,7 @@ var elevator: CharacterBody3D
 @onready var left_ray: RayCast3D = $CollisionRayLeft
 @onready var right_ray: RayCast3D = $CollisionRayRight
 @onready var body: CSGMesh3D = $Body
+@onready var elevator_ding: AudioStreamPlayer = $ElevatorDing
 
 
 func _ready() -> void:
@@ -44,6 +45,7 @@ func enter_elevator() -> void:
 		in_elevator = true
 		has_ever_entered_elevator = true
 		elevator.number_of_people_in_elevator += 1
+		elevator_ding.play()
 
 
 func drop_off(corridor_colour: Color, new_direction: int) -> void:
@@ -53,6 +55,7 @@ func drop_off(corridor_colour: Color, new_direction: int) -> void:
 		in_elevator = false
 		get_tree().root.get_child(0).score += 100
 		elevator.number_of_people_in_elevator -= 1
+		elevator_ding.play()
 		#think about what happens after dropoff
 		#maybe they just dissapear when they hit the next wall or something
 		#queue_free()
