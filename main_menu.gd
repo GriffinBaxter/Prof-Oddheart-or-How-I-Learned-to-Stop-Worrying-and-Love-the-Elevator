8ed3c546254bb0df33738b943c91a1885db98c59
keagan erasmus <keaganerasmus12@gmail.com>
2025-04-18 19:59:06 +1200

    Update main_menu.gd

--- main_menu.gd
+++ main_menu.gd
@@ -1,7 +1,5 @@
 extends Node3D
 
-@onready var game_scene = preload("res://main.tscn").instantiate()
-
 func _on_go_to_play_body_entered(body: Node3D) -> void:
 	if body.is_in_group("elevator"):
 		get_tree().change_scene_to_file("res://main.tscn")
