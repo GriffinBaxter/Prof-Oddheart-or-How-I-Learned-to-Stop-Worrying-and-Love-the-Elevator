836e0e6bf6a25c50e0a40a186a9daddb0a9ab1f3
keagan erasmus <keaganerasmus12@gmail.com>
2025-04-18 20:11:34 +1200

    Added Elevator music

--- main_menu.gd
+++ main_menu.gd
@@ -1,5 +1,10 @@
 extends Node3D
 
+@onready var sound_player = $MainSoundPlayer
+
+func _ready() -> void:
+	sound_player.play()
+
 func _on_go_to_play_body_entered(body: Node3D) -> void:
 	if body.is_in_group("elevator"):
 		get_tree().change_scene_to_file("res://main.tscn")
