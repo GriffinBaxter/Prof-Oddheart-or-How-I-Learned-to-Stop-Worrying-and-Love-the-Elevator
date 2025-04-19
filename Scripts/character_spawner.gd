b6459e205bd9078f153c67c88b1e40bd56d215eb
keagan erasmus <keaganerasmus12@gmail.com>
2025-04-20 08:49:18 +1200

    Cleaned up some of the old spawner code

--- Scripts/character_spawner.gd
+++ Scripts/character_spawner.gd
@@ -1,18 +1,13 @@
 extends Node3D
 
 @export var character_scene: PackedScene
-@export var min_spawn_time: float
-@export var max_spawn_time: float
 
-@onready var timer: Timer = $SpawnTimer
 @onready var csg_mesh_3d: CSGMesh3D = $CSGMesh3D
 
 
 func _ready() -> void:
 	csg_mesh_3d.hide()
 
-
 func spawn() -> void:
 	var character := character_scene.instantiate()
-	timer.wait_time = randf_range(min_spawn_time, max_spawn_time)
 	add_child(character)
