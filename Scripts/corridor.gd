8c3a0b026c648eebde6527188d61fd17dac21380
Griffin Baxter <47467946+GriffinBaxter@users.noreply.github.com>
2025-04-20 19:57:29 +1200

    Make front of corridors translucent

--- Scripts/corridor.gd
+++ Scripts/corridor.gd
@@ -3,6 +3,7 @@ extends Node3D
 var colour: Color = [Color.RED, Color.GREEN, Color.BLUE].pick_random()
 
 @onready var corridor_boxes: Array[CSGBox3D] = [$Back, $Top, $Bottom]
+@onready var corridor_box_transparent: CSGBox3D = $Front
 
 
 func _ready() -> void:
@@ -10,6 +11,10 @@ func _ready() -> void:
 	material.albedo_color = colour
 	for box in corridor_boxes:
 		box.material = material
+	var transparent_material := StandardMaterial3D.new()
+	transparent_material.albedo_color = colour - Color(0, 0, 0, 0.75)
+	transparent_material.transparency = BaseMaterial3D.TRANSPARENCY_ALPHA
+	corridor_box_transparent.material = transparent_material
 
 
 func _on_drop_off_area_left_body_entered(body: Node3D) -> void:
